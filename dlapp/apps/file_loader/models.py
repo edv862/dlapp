511ae95f5f2cca82e687c8de76282477ce97af7f
Sermed92 <sermed19@gmail.com>
2017-12-29 16:46:29 -0400

    model output added

--- dlapp/apps/file_loader/models.py
+++ dlapp/apps/file_loader/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class output(models.Model):
+    # Usuario que creo el output
+    owner = models.ForeignKey(
+        User,
+        null=False,
+        on_delete=models.CASCADE
+    )
+    date = models.DateTimeField(
+        auto_now_add=True,
+        blank=True
+    )
+    output_text = models.TextField(
+        u'Texto extraido',
+        blank=True,
+        null=True
+    )
