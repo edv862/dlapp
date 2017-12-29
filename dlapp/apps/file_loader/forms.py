e7aa7e7edfd2b2d56ae2b3e1440a535b694c7e81
Sermed92 <sermed19@gmail.com>
2017-12-29 17:12:35 -0400

    forms choices

--- dlapp/apps/file_loader/forms.py
+++ dlapp/apps/file_loader/forms.py
@@ -2,6 +2,18 @@ from django import forms
 
 
 class FileUploadForm(forms.Form):
+    line = "ln"
+    parag = "pg"
+    TYPE_CHOICES = (
+        (line, 'Line'),
+        (parag, 'Paragraph'),
+    )
+
+    choice_type = forms.ChoiceField(
+        choices=TYPE_CHOICES,
+        required=True
+    )
+
     file = forms.FileField(
         widget=forms.ClearableFileInput(
             attrs={
